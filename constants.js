56ace001ea7ed90a909ae704b15b221d36e2eb7b
donsweeney4 <donsweeney4@gmail.com>
2025-05-08 09:38:33 -0700

    initial case for V4.0.0

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // constants.js
 export const LOCATION_TASK_NAME = "background-location-task";
 export const TARGET_CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-export const VERSION = "3.0.3";
+export const VERSION = "4.0.0";
 
 
 
